efcf59352b46f9ff1e4c37d422ef81478d89ab2a
knut tungland <stunglan@gmail.com>
2017-04-29 10:52:54 +0200

    first pass

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -52,6 +52,70 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+  ///* initially set to false, set to true in first call of ProcessMeasurement
+  is_initialized_ = false;
+  
+ 
+  
+ 
+  
+  ///* time when the state is true, in us
+  //time_us_ = 0;
+  
+  ///* Process noise standard deviation longitudinal acceleration in m/s^2
+  std_a_ = 3.0;
+  
+  ///* Process noise standard deviation yaw acceleration in rad/s^2
+  std_yawdd_ = 0.6;
+  
+  ///* Laser measurement noise standard deviation position1 in m
+  std_laspx_ = 0.6;
+  
+  ///* Laser measurement noise standard deviation position2 in m
+  std_laspy_ = 0.15;
+  
+  ///* Radar measurement noise standard deviation radius in m
+  std_radr_ = 0.3;
+  
+  ///* Radar measurement noise standard deviation angle in rad
+  std_radphi_ = 0.03;
+  
+  ///* Radar measurement noise standard deviation radius change in m/s
+  std_radrd_ = 0.3;
+  
+  
+  ///* State dimension
+  n_x_ = 5;
+  
+  ///* Augmented state dimension
+  n_aug_ = 7;
+  
+  ///* Sigma point spreading parameter
+  lambda_ = 3 - n_aug_;;
+  
+  ///* the current NIS for radar
+  NIS_radar_ = 0.0;
+  
+  ///* the current NIS for laser
+  NIS_laser_ = 0.0;
+  
+  ///* Weights of sigma points
+  weights_ = VectorXd(2*n_aug_+1);
+  
+  // code from quiz
+  double weight_0 = lambda_/(lambda_+n_aug_);
+  weights_(0) = weight_0;
+  for (int i=1; i<2*n_aug_+1; i++) {  //2n+1 weights
+    double weight = 0.5/(n_aug_+lambda_);
+    weights_(i) = weight;
+  }
+  
+  
+  
+  ///* predicted sigma points matrix
+  Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);
+  Xsig_pred_.fill(0.0);
+
 }
 
 UKF::~UKF() {}
